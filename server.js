9aba2022323f56f60c89fe5fd0678aafde27e26b
memesis679 <stepanmors2014@gmail.com>
2026-04-19 10:14:57 +0300

    server.js

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const ytdl = require('ytdl-core');
 
+const app = express();
+app.use(cors());
+
+app.get('/download', (req, res) => {
+    const url = req.query.url;
+
+    if (!ytdl.validateURL(url)) {
+        return res.send("bad url");
+    }
+
+    res.header('Content-Disposition', 'attachment; filename="video.mp4"');
+
+    ytdl(url, { quality: 'highest' }).pipe(res);
+});
+
+app.listen(3000, () => {
+    console.log("Server running");
+});
